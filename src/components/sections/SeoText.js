e2c0684d8895a01110ee799fe82fa21d737047a0
Elias Bengtsson <eliasbengtsson11@gmail.com>
2026-08-03 15:32:03 +0200

    accessibility #3

--- src/components/sections/SeoText.js
+++ src/components/sections/SeoText.js
@@ -3,7 +3,7 @@ export default function SeoText() {
     <section className="my-24 md:my-32 opacity-30 hover:opacity-100 transition-opacity duration-200">
       <div className="max-w-7xl mx-auto px-6 md:px-12">
         <div className="mx-auto max-w-4xl text-center">
-          <p className="text-xs text-muted-text font-normal leading-relaxed">
+          <p className="text-xs text-foreground-text font-light leading-relaxed">
             Elhjälp Sverige AB är din lokala partner för elinstallationer i Borås och Sjuhäradsbygden med omnnejd. 
             Vi är ett elföretag registrerat hos Elsäkerhetsverket vilket innebär att alla våra 
             tjänster utförs enligt högsta säkerhetskrav.
